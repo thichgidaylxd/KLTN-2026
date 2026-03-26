4e438eb609e34e0ee50ca9fe7df8bdddba0ec326
Dũng <lxd0294@gmail.com>
2026-03-26 10:44:31 +0700

    remove Dashboard in publicRoute

--- src/routes/publicRoutes.ts
+++ src/routes/publicRoutes.ts
@@ -5,8 +5,6 @@ const Home = lazy(() => import("../pages/Home/Home"));
 const Register = lazy(() => import("../pages/Register/Register"));
 const Login = lazy(() => import("../pages/Login/Login"));
 const About = lazy(() => import("../pages/About/About"));
-const Dashboard = lazy(() => import("../pages/Dashboard/Dashboard"));
-
 /**
  * Public Routes - Không yêu cầu đăng nhập
  * Ai cũng có thể truy cập
